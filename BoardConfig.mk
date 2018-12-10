b93b7eb2e9db7496ca503b13ecfce4d49575da9a
Kilian von Pflugk <github@jumoog.io>
2018-12-10 12:31:27 +0100

    surya: Remove TARGET_USES_64_BIT_BINDER
    The TARGET_USES_64_BIT_BINDER option is deprecated and can be removed
    
    Change-Id: I6b828da1fbcc8c054c5b35ff78c870a689f7ed9f
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,8 +22,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a76
 
-TARGET_USES_64_BIT_BINDER := true
-
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
