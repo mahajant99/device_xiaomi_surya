ef4b76d25f4e6550a32bafb4760072b8fdd2a931
Pranav Vashi <neobuddy89@gmail.com>
2020-04-11 17:12:27 +0530

    phoenix: Update props
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>
    Change-Id: I418d9749bb7c0c5e9d08d0bedcb955d831e32404

--- product_prop.mk
+++ product_prop.mk
@@ -19,13 +19,80 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.camera.privapp.list=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
 
-# Telephony
+# CNE
 PRODUCT_PRODUCT_PROPERTIES += \
+    persist.vendor.cne.feature=1
+
+# Data
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.data.df.dev_name=rmnet_usb0 \
+    persist.vendor.data.profile_update=true \
+    persist.vendor.data.mode=concurrent \
+    ro.vendor.use_data_netmgrd=true
+
+# Display
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.bl.poll=true \
+    ro.vendor.display.ad=1 \
+    ro.vendor.display.ad.sdr_calib_data=/vendor/etc/sdr_config.cfg \
+    ro.vendor.display.ad.hdr_calib_data=/vendor/etc/hdr_config.cfg \
+    ro.vendor.display.sensortype=2 \
+    ro.colorpick_adjust=true \
+    ro.all_modes.colorpick_adjust=true \
+    ro.vendor.cabc.enable=true \
+    ro.vendor.bcbc.enable=false \
+    ro.vendor.dfps.enable=true \
+    ro.vendor.display.default_fps=60 \
+    vendor.display.idle_time=1100 \
+    sys.displayfeature_hidl=true \
+    ro.sf.lcd_density=440
+
+# DPM
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.vendor.dpm.feature=1 \
+    persist.vendor.dpm.loglevel=0 \
+    persist.vendor.dpm.nsrm.bkg.evt=3955
+
+# GPS
+PRODUCT_PRODUCT_PROPERTIES += \
+    sys.qca1530=detect
+
+# Memory
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.qti.sys.fw.bservice_enable=true
+
+# Netflix
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.netflix.bsp_rev=Q6150-17263-1
+
+# Radio
+PRODUCT_PRODUCT_PROPERTIES += \
+    DEVICE_PROVISIONED=1 \
+    persist.sys.fflag.override.settings_network_and_internet_v2=true \
+    persist.vendor.ims.disableADBLogs=1 \
+    persist.vendor.ims.disableIMSLogs=1 \
+    persist.vendor.radio.enable_temp_dds=true \
+    persist.vendor.radio.force_on_dc=true \
+    persist.vendor.radio.uicc_se_enabled=false \
+    ril.subscription.types=RUIM \
+    persist.radio.multisim.config=dsds \
+    persist.radio.VT_CAM_INTERFACE=1 \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.data_con_rprt=1 \
+    persist.vendor.radio.data_ltd_sys_ind=1 \
+    persist.vendor.radio.force_ltd_sys_ind=1 \
+    persist.vendor.radio.procedure_bytes=SKIP \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.redir_party_num=1 \
+    persist.vendor.radio.sib16_support=1 \
     ro.telephony.default_network=22,22 \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1 \
-    persist.sys.fflag.override.settings_network_and_internet_v2=true
+    telephony.lteOnCdmaDevice=1 \
+    vendor.rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so
+
+# RCS
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.rcs.supported=0
 
 # Wireless display
 PRODUCT_PRODUCT_PROPERTIES += \
