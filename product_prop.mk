0439dedc51f9814fdaf70aaae24deff46fe0a739
Pranav Vashi <neobuddy89@gmail.com>
2020-04-13 13:13:50 +0530

    phoenix: Add props for graphics
    * Kanged few from Pixel 4 and RazerPhone
    * Some of these are already enabled in vendor partition.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>
    Change-Id: I9b09e91f840897e4d95b5dc76527d42642481b6f

--- product_prop.mk
+++ product_prop.mk
@@ -45,6 +45,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.display.default_fps=60 \
     vendor.display.idle_time=1100 \
     sys.displayfeature_hidl=true \
+    vendor.hbm.enable=true \
     ro.sf.lcd_density=440
 
 # DPM
@@ -56,6 +57,47 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # GPS
 PRODUCT_PRODUCT_PROPERTIES += \
     sys.qca1530=detect
+
+# Graphics
+PRODUCT_PRODUCT_PROPERTIES += \
+    debug.egl.hw=0 \
+    debug.mdpcomp.logs=0 \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.early_app_phase_offset_ns=1500000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_phase_offset_ns=1500000 \
+    debug.sf.high_fps_early_phase_offset_ns=6100000 \
+    debug.sf.high_fps_early_gl_phase_offset_ns=9000000 \
+    debug.sf.hw=0 \
+    debug.sf.phase_offset_threshold_for_next_vsync_ns=6100000 \
+    persist.demo.hdmirotationlock=false \
+    persist.hwc.enable_vds=1 \
+    persist.sys.sf.color_saturation=1.0 \
+    ro.opengles.version=196610 \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+    ro.surface_flinger.wcg_composition_dataspace=143261696 \
+    vendor.display.comp_mask=0 \
+    vendor.display.dataspace_saturation_matrix=1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0 \
+    vendor.display.disable_decimation=1 \
+    vendor.display.disable_excl_rect=0 \
+    vendor.display.disable_hw_recovery_dump=0 \
+    vendor.display.disable_inline_rotator=1 \
+    vendor.display.disable_scaler=0 \
+    vendor.display.disable_ui_3d_tonemap=1 \
+    vendor.display.enable_default_color_mode=1 \
+    vendor.display.enable_force_split=0 \
+    vendor.display.enable_null_display=0 \
+    vendor.display.enable_optimize_refresh=1 \
+    vendor.gralloc.disable_ubwc=0
 
 # Memory
 PRODUCT_PRODUCT_PROPERTIES += \
