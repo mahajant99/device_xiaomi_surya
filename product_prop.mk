3fe869e3789f9290f1b75192ee116c986c0e1856
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-18 23:20:50 -0700

    surya: Force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable
    impact on latency.
    
    Change-Id: I4d1de21487757be3df3bd908e1a6152fd951d732
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -28,6 +28,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
 
 # Smart fps switching
