2244fd1eb79adcb1827d737077fbec55501a2ac0
TheScarastic <warabhishek@gmail.com>
2020-05-09 08:23:43 +0000

    surya: parts: Add dynamic thermal profile implementation
    Also add in FileUtils
    
    Change-Id: I4667b24a8a29a418796dc51b245ede0aca51eea6
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,6 +24,7 @@ import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -35,5 +36,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize();
         DozeUtils.checkDozeService(context);
+        ThermalUtils.startService(context);
     }
 }
