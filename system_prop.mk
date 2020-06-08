f2e054ef651167a6feaa9fce5c3d9f41236fcefe
tangrobin <tangrobin@google.com>
2020-06-08 23:11:12 +0800

    surya: Use phase offsets as duration
    In Android R, the SurfaceFlinger calculate phase offsets
    based on duration. Adopt the new way of specifying
    phase offsets to SurfaceFlinger.
    
    Bug: 158464992
    Test: scrolling, app launching/closing, playing video
    Change-Id: Ia717f5c0075b6adaedaf20dd6be689737edc1415
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -23,3 +23,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Zygote
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
+
+# Enable app/sf phase offset as durations. The numbers below are translated from the existing
+# positive offsets by finding the duration app/sf will have with the offsets.
+# For SF the previous value was 6ms which under 16.6ms vsync time (60Hz) will leave SF with ~10.5ms
+# for each frame. For App the previous value was 2ms which under 16.6ms vsync time will leave the
+# App with ~20.5ms (16.6ms * 2 - 10.5ms - 2ms). The other values were calculated similarly.
+# Full comparison between the old vs. the new values are captured in
+# https://docs.google.com/spreadsheets/d/1a_5cVNY3LUAkeg-yL56rYQNwved6Hy-dvEcKSxp6f8k/edit
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.use_phase_offsets_as_durations=1
+    debug.sf.late.sf.duration=10500000
+    debug.sf.late.app.duration=20500000
+    debug.sf.early.sf.duration=21000000
+    debug.sf.early.app.duration=16500000
+    debug.sf.earlyGl.sf.duration=13500000
+    debug.sf.earlyGl.app.duration=21000000
