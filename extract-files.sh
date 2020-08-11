26d4d1a917c473050f0181d1e7e41698fc7b54ce
kazerine <kamikaze.mrinal@gmail.com>
2020-08-12 00:18:36 +0530

    surya: Get rid of megvii dependencies in camera.qcom.so
    Change-Id: I97058c320d06819a468ebe38e2704360e9aa91db
    Signed-off-by: Arian <arian.kulmer@web.de>

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/hw/camera.qcom.so)
+            "${PATCHELF}" --remove-needed "libMegviiFacepp-0.5.2.so" "${2}"
+            "${PATCHELF}" --remove-needed "libmegface.so" "${2}"
+            "${PATCHELF}" --add-needed "libshim_megvii.so" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
