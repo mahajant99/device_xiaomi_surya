6132ce26baa3992ae46770aefcd43ec80e96550d
GtrCraft <nickvanbokhorst@hotmail.com>
2020-08-16 17:25:00 +0200

    phoenix: Update camera props - Fix Aux
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -14,7 +14,10 @@ endif
 
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
-    vendor.camera.aux.packagelist=org.lineageos.snap,net.sourceforge.opencamera,org.codeaurora.snapcam
+    persist.camera.gyro.disable=0 \
+    persist.camera.privapp.list=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
+    persist.vendor.camera.privapp.list=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
 
 # Telephony
 PRODUCT_PRODUCT_PROPERTIES += \
