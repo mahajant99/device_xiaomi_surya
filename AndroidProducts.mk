b474507e0c8f1a48e909b9bc4591cceaf115286e
Tushar Mahajan <mahajant99@gmail.com>
2020-09-29 17:36:00 +0530

    surya: Havoc-OS Bringup
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_surya.mk
+    $(LOCAL_DIR)/havoc_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_surya-user \
-    lineage_surya-userdebug \
-    lineage_surya-eng
+    havoc_surya-user \
+    havoc_surya-userdebug \
+    havoc_surya-eng
