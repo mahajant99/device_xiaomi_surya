880497f065746ef3b3c3e57be7d9f04e720e9664
Park Ju Hyung <qkrwngud825@gmail.com>
2020-10-09 09:25:39 +0300

    surya: init: workaround SafetyNet check
    Doing this in the userspace allows more properties to be spoofed
    and eliminate the needs for a hack in the kernel.

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -34,6 +34,8 @@
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/_system_properties.h>
 
 #include "property_service.h"
@@ -57,6 +59,50 @@ void property_override(char const prop[], char const value[], bool add = true) {
         __system_property_update(pi, value, strlen(value));
     else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+/* From Magisk@jni/magiskhide/hide_utils.c */
+static const char *snet_prop_key[] = {
+    "ro.boot.vbmeta.device_state",
+    "ro.boot.verifiedbootstate",
+    "ro.boot.flash.locked",
+    "ro.boot.selinux",
+    "ro.boot.veritymode",
+    "ro.boot.warranty_bit",
+    "ro.warranty_bit",
+    "ro.debuggable",
+    "ro.secure",
+    "ro.build.type",
+    "ro.build.tags",
+    "ro.build.selinux",
+    NULL
+};
+
+static const char *snet_prop_value[] = {
+    "locked",
+    "green",
+    "1",
+    "enforcing",
+    "enforcing",
+    "0",
+    "0",
+    "0",
+    "1",
+    "user",
+    "release-keys",
+    "1",
+    NULL
+};
+
+static void workaround_snet_properties() {
+
+    // Hide all sensitive props
+    for (int i = 0; snet_prop_key[i]; ++i) {
+        property_override(snet_prop_key[i], snet_prop_value[i]);
+    }
+
+    chmod("/sys/fs/selinux/enforce", 0640);
+    chmod("/sys/fs/selinux/policy", 0440);
 }
 
 void vendor_load_properties() {
@@ -84,4 +130,7 @@ void vendor_load_properties() {
     property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
     property_override("ro.bootimage.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
     property_override("ro.build.description", "surya_global-user 10 QKQ1.200512.002 V12.0.2.0.QJGMIXM release-keys");
+
+    // Workaround SafetyNet
+    workaround_snet_properties();
 }
