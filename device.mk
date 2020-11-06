07066029c13d86787daddc1cc194bc84e1635d3d
Tushar Mahajan <mahajant99@gmail.com>
2020-11-06 23:02:18 +0530

    surya: Bind mount and disable NQNfcNci for karna
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- device.mk
+++ device.mk
@@ -91,7 +91,8 @@ PRODUCT_COPY_FILES += \
 # Init
 PRODUCT_PACKAGES += \
     init.mi_thermald.rc \
-    init.device.rc
+    init.device.rc \
+    init.qcom.rc
 
 # Keylayout
 PRODUCT_COPY_FILES += \
