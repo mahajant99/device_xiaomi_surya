ea351ebf61362207d47c56807f4daeecd19b5cf4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-02 16:57:24 +0530

    surya: Switch to 90Hz refresh rate by default
     * a sweet spot between 60Hz (battery life) and 120Hz (smoothness)
    
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- parts/src/org/lineageos/settings/utils/RefreshRateUtils.java
+++ parts/src/org/lineageos/settings/utils/RefreshRateUtils.java
@@ -26,7 +26,7 @@ public class RefreshRateUtils {
 
     public static int getRefreshRate(Context context) {
         SharedPreferences sharedPref = context.getSharedPreferences("parts_pref", Context.MODE_PRIVATE);
-        return sharedPref.getInt("refresh_rate", 2);
+        return sharedPref.getInt("refresh_rate", 3);
     }
 
     public static void setRefreshRate(Context context, int refreshRate) {
