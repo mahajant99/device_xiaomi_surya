f722576617df639b77857120ccd66271da62ae3a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-13 12:40:23 +0530

    surya: Cleanup and update init extension
     * switch to sunfish description
     * remove unused safetynet hacks and nfc check
     * fix wrong ram size check logic
     * code style nitpicks
    
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -46,15 +46,9 @@
 
 using android::base::GetProperty;
 using android::base::SetProperty;
+using std::string;
 
-char const *heapstartsize;
-char const *heapgrowthlimit;
-char const *heapsize;
-char const *heapminfree;
-char const *heapmaxfree;
-char const *heaptargetutilization;
-
-std::vector<std::string> ro_props_default_source_order = {
+std::vector<string> ro_props_default_source_order = {
     "",
     "product.",
     "system.",
@@ -65,15 +59,16 @@ void property_override(char const prop[], char const value[], bool add = true) {
     prop_info *pi;
 
     pi = (prop_info *)__system_property_find(prop);
+
     if (pi)
-    __system_property_update(pi, value, strlen(value));
+        __system_property_update(pi, value, strlen(value));
     else if (add)
-    __system_property_add(prop, strlen(prop), value, strlen(value));
+        __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void set_ro_build_prop(const std::string &source, const std::string &prop,
-                       const std::string &value, bool product = false) {
-    std::string prop_name;
+void set_ro_build_prop(const string &source, const string &prop,
+                       const string &value, bool product = false) {
+    string prop_name;
 
     if (product) {
         prop_name = "ro.product." + source + prop;
@@ -84,8 +79,8 @@ void set_ro_build_prop(const std::string &source, const std::string &prop,
     property_override(prop_name.c_str(), value.c_str(), false);
 }
 
-void set_device_props(const std::string fingerprint, const std::string description,
-                      const std::string brand, const std::string device, const std::string model) {
+void set_device_props(const string fingerprint, const string description,
+                      const string brand, const string device, const string model) {
     for (const auto &source : ro_props_default_source_order) {
         set_ro_build_prop(source, "fingerprint", fingerprint);
         set_ro_build_prop(source, "brand", brand, true);
@@ -97,110 +92,32 @@ void set_device_props(const std::string fingerprint, const std::string descripti
     property_override("ro.build.description", description.c_str());
 }
 
-/* From Magisk@jni/magiskhide/hide_utils.c */
-static const char *snet_prop_key[] = {
-    "ro.boot.vbmeta.device_state",
-    "ro.boot.verifiedbootstate",
-    "ro.boot.flash.locked",
-    "ro.boot.selinux",
-    "ro.boot.veritymode",
-    "ro.boot.warranty_bit",
-    "ro.warranty_bit",
-    "ro.debuggable",
-    "ro.secure",
-    "ro.build.type",
-    "ro.build.tags",
-    "ro.build.selinux",
-    NULL
-};
-
-static const char *snet_prop_value[] = {
-    "locked",
-    "green",
-    "1",
-    "enforcing",
-    "enforcing",
-    "0",
-    "0",
-    "0",
-    "1",
-    "user",
-    "release-keys",
-    "1",
-    NULL
-};
-
-static void workaround_snet_properties() {
-
-    // Hide all sensitive props
-    for (int i = 0; snet_prop_key[i]; ++i) {
-        property_override(snet_prop_key[i], snet_prop_value[i]);
-    }
-
-    chmod("/sys/fs/selinux/enforce", 0640);
-    chmod("/sys/fs/selinux/policy", 0440);
-}
-
 void load_device_properties() {
-    std::string hwname = GetProperty("ro.boot.hwname", "");
-    std::string region = GetProperty("ro.boot.hwc", "");
+    string hwname = GetProperty("ro.boot.hwname", "");
+
+    string fingerprint = "google/sunfish/sunfish:11/RQ1A.210105.002/6985033:user/release-keys";
+    string description = "sunfish-user 11 RQ1A.210105.002 6985033 release-keys";
 
     if (hwname == "surya") {
-        if (region == "INT") {
-            set_device_props(
-                             "google/sunfish/sunfish:11/RQ1A.210105.002/6985033:user/release-keys",
-                             "surya_global-user 10 QKQ1.200512.002 V12.0.6.0.QJGMIXM release-keys",
-                             "Poco", "surya", "M2007J20CG");
-        } else if (region == "India") {
-            set_device_props(
-                             "google/sunfish/sunfish:11/RQ1A.210105.002/6985033:user/release-keys",
-                             "surya_in-user 10 QKQ1.200512.002 V12.0.8.0.QJGINXM release-keys",
-                             "Poco", "surya", "M2007J20CG");
-        }
+        set_device_props(fingerprint, description, "Poco", "surya", "M2007J20CG");
     } else if (hwname == "karna") {
-        if (region == "INT") {
-            set_device_props(
-                             "google/sunfish/sunfish:11/RQ1A.210105.002/6985033:user/release-keys",
-                             "karna_global-user 10 QKQ1.200512.002 V12.0.6.0.QJGMIXM release-keys",
-                             "Poco", "karna", "M2007J20CI");
-        } else if (region == "India") {
-            set_device_props(
-                             "google/sunfish/sunfish:11/RQ1A.210105.002/6985033:user/release-keys",
-                             "karna_in-user 10 QKQ1.200512.002 V12.0.8.0.QJGINXM release-keys",
-                             "Poco", "karna", "M2007J20CI");
-        }
+        set_device_props(fingerprint, description, "Poco", "karna", "M2007J20CI");
     }
 }
 
-
-void nfc_check()
-{
-    std::string hwname = GetProperty("ro.boot.hwname", "");
-
-    char nfc_prop[] = "ro.hw.nfc";
-
-    if (hwname == "surya"){
-        property_override(nfc_prop, "1");
-    } else if (hwname == "karna"){
-        property_override(nfc_prop, "0");
-    }
-}
-
-void check_device()
+void load_dalvik_properties()
 {
     struct sysinfo sys;
+    char const *heapstartsize;
+    char const *heapgrowthlimit;
+    char const *heapsize;
+    char const *heapminfree;
+    char const *heapmaxfree;
+    char const *heaptargetutilization;
 
     sysinfo(&sys);
 
-    if (sys.totalram >= 5ull * 1024 * 1024 * 1024){
-        // from - phone-xhdpi-6144-dalvik-heap.mk
-        heapstartsize = "16m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.5";
-        heapminfree = "8m";
-        heapmaxfree = "32m";
-    } else if (sys.totalram >= 7ull * 1024 * 1024 * 1024) {
+    if (sys.totalram >= 7ull * 1024 * 1024 * 1024) {
         // from - phone-xhdpi-8192-dalvik-heap.mk
         heapstartsize = "24m";
         heapgrowthlimit = "256m";
@@ -208,12 +125,15 @@ void check_device()
         heaptargetutilization = "0.46";
         heapminfree = "8m";
         heapmaxfree = "48m";
+    } else if (sys.totalram >= 5ull * 1024 * 1024 * 1024){
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.5";
+        heapminfree = "8m";
+        heapmaxfree = "32m";
     }
-}
-
-void vendor_load_properties()
-{
-    check_device();
 
     SetProperty("dalvik.vm.heapstartsize", heapstartsize);
     SetProperty("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
@@ -221,11 +141,10 @@ void vendor_load_properties()
     SetProperty("dalvik.vm.heaptargetutilization", heaptargetutilization);
     SetProperty("dalvik.vm.heapminfree", heapminfree);
     SetProperty("dalvik.vm.heapmaxfree", heapmaxfree);
+}
 
+void vendor_load_properties()
+{
+    load_dalvik_properties();
     load_device_properties();
-
-    nfc_check();
-
-    // Workaround SafetyNet
-    workaround_snet_properties();
 }
