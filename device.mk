3f734f25195daefbaed7ece7747161be030ec49e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-21 18:30:12 +0530

    surya: Get rid of google recorder
     * after removing fluence props, it no longer works
     * based on https://github.com/motoz2-force/android_device_motorola_nash/commit/9833b3b453a5b0c5cdb7631b2e023d375b67c9a8
    
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- device.mk
+++ device.mk
@@ -157,6 +157,10 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
+# Remove Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
