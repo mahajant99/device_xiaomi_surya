8ed6c478f6900624dc80b070168a0ac5db6b6373
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-01-29 15:51:07 +0300

    surya: Force disable wide color display
    Xiaomi has built this device with TARGET_HAS_WIDE_COLOR_DISPLAY=true (possibly because it is the default in caf), but the hardware doesn't properly support DISPLAY_P3 profilem causing weird yellowish tint in some scenarios. Force disable the feature to fix it.
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -37,6 +37,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     vendor.display.disable_rotator_downscale=1 \
+    ro.surface_flinger.has_wide_color_display=false
 
 # Smart fps switching
 PRODUCT_PRODUCT_PROPERTIES += \
