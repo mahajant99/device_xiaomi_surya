b0bc4aacb6451d0f28b1fcabbfb82a1e6cc8668b
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-02-03 23:46:01 +0300

    surya: Don't remove recorder
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- device.mk
+++ device.mk
@@ -150,10 +150,6 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
-# Remove Packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
