24a3d6d71964557cb339d86adac7bcfeb5d33b55
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-09 01:32:05 +0530

    surya: Load Q libsnsapi.so for camera blobs
    Fixes:
    
    03-09 00:37:36.875  4256  4256 E vndksupport: Could not load /vendor/lib64/hw/camera.qcom.so from default namespace: dlopen failed: cannot locate symbol "_ZN20sns_client_event_msg17default_instance_E" referenced by "/vendor/lib64/hw/camera.qcom.so"....

--- extract-files.sh
+++ extract-files.sh
@@ -13,6 +13,13 @@ function blob_fixup() {
             "${PATCHELF}" --remove-needed "libmegface.so" "${2}"
             "${PATCHELF}" --add-needed "libshim_megvii.so" "${2}"
             "${PATCHELF}" --add-needed "libcomparetf2.so" "${2}"
+            sed -i 's/libsnsapi/libhaxapi/g' "${2}"
+            ;;
+        vendor/lib64/libcamera_nn_stub.so|vendor/lib64/camera/components/com.qti.node.eisv3.so|vendor/lib64/camera/components/com.qti.camx.chiiqutils.so|vendor/lib64/camera/components/libmmcamera_cac3.so|vendor/lib64/camera/components/com.qti.node.eisv2.so)
+            sed -i 's/libsnsapi/libhaxapi/g' "${2}"
+            ;;
+        vendor/lib64/libhaxapi.so )
+            "${PATCHELF}" --set-soname "libhaxapi.so" "${2}"
             ;;
         vendor/lib64/libvidhance.so|vendor/lib64/camera/components/com.vidhance.node.eis.so)
             "${PATCHELF}" --add-needed "libcomparetf2.so" "${2}"
