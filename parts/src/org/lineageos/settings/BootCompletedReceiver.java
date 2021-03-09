133039ccb9e3d2abf9baa936b6f794c19ed1b3e6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-09 18:39:39 +0530

    surya: parts: Remove refresh rate settings
     * we use dynamic refresh rate + aosp's way of setting min refresh rate

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,24 +20,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.PowerManager;
 
-import org.lineageos.settings.PowerSaveModeChangeReceiver;
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
-import org.lineageos.settings.utils.RefreshRateUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        // Refresh rate
-        RefreshRateUtils.setFPS(RefreshRateUtils.getRefreshRate(context));
-        IntentFilter filter = new IntentFilter();
-        PowerSaveModeChangeReceiver receiver = new PowerSaveModeChangeReceiver();
-        filter.addAction(PowerManager.ACTION_POWER_SAVE_MODE_CHANGED);
-        context.getApplicationContext().registerReceiver(receiver, filter);
-
         // Dirac
         DiracUtils.initialize(context);
 
