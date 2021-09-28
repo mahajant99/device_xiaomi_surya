039ed12fedd4cb60fd4f27e90b03c1c78a677662
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-09-28 13:37:24 +0530

    sm6150-common: Nuke msm_irqbalance
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -202,10 +202,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/security/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
-# IRQ balance
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.xiaomi_sm6150
