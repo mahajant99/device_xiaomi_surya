908dcfe82fe4b6c12e685f44171ee4bf545a7e65
Tushar Mahajan <mahajant99@gmail.com>
2022-04-02 20:30:36 +0530

    [DNM]surya: Set neverallows to true
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,6 +195,8 @@ TARGET_SCREEN_DENSITY := 420
 VENDOR_SECURITY_PATCH := 2021-07-01
 
 # Sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
+
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
