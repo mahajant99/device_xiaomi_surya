20f18696043edc6295de293e345a3370469a54f0
Kuba Wojciechowski <nullbytepl@gmail.com>
2022-06-10 20:36:28 +0530

    surya: Disable kpti
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,6 +128,7 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += kpti=off
 
 # Media
 TARGET_USES_ION := true
